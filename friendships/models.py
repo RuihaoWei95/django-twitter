27e8efe74860cc8ee38a67a56aec1b49bdda396d
RuihaoWei95 <74844130+RuihaoWei@users.noreply.github.com>
2022-05-27 17:39:37 -0700

    updated more untracked file

---
+++ friendships/models.py
@@ -0,0 +1,32 @@
+from django.db import models
+from django.contrib.auth.models import User
+# Create your models here.
+
+
+class Friendship(models.Model):
+    from_user = models.ForeignKey(
+        User,
+        on_delete=models.SET_NULL,
+        null=True,
+        related_name='following_friendship_set',
+    )
+    to_user = models.ForeignKey(
+        User,
+        on_delete=models.SET_NULL,
+        null=True,
+        related_name='follower_friendship_set',
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        index_together = (
+            # get all people I followed, order by time
+            ('from_user_id', 'created_at'),
+            # get all people followed me, order by time
+            ('to_user_id', 'created_at'),
+        )
+        unique_together = (('from_user_id', 'to_user_id'),)
+
+    def __str__(self):
+        return f'{self.from_user_id} followed {self.to_user_id}'
+

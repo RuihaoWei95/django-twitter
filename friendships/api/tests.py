9a0f4958e171e79a1ce75271aabf30c2c1971173
RuihaoWei95 <74844130+RuihaoWei@users.noreply.github.com>
2022-05-27 17:26:03 -0700

    8-friendships-model-api-and-tests

---
+++ friendships/api/tests.py
@@ -0,0 +1,147 @@
+from friendships.models import Friendship
+from testing.testcases import TestCase
+from rest_framework.test import APIClient
+from rest_framework import status
+
+FOLLOW_URL = '/api/friendships/{}/follow/'
+FOLLOWINGS_URL = '/api/friendships/{}/followings/'
+UNFOLLOW_URL = '/api/friendships/{}/unfollow/'
+FOLLOWERS_URL = '/api/friendships/{}/followers/'
+
+
+class FriendshipApiTest(TestCase):
+
+    def setUp(self):
+        self.anonymous_client = APIClient()
+
+        self.gongzi = self.create_user('gongzi')
+        self.gongzi_client = APIClient()
+        self.gongzi_client.force_authenticate(self.gongzi)
+
+        self.xiaoweige = self.create_user('xiaoweige')
+        self.xiaoweige_client = APIClient()
+        self.xiaoweige_client.force_authenticate(self.xiaoweige)
+
+        # create following and followers
+        for i in range(2):
+            follower = self.create_user(f'xiaoweige_follower{i}')
+            Friendship.objects.create(from_user=follower, to_user=self.xiaoweige)
+        for i in range(3):
+            following = self.create_user(f'xiaoweige_following{i}')
+            Friendship.objects.create(from_user=self.xiaoweige, to_user=following)
+
+    def test_follow(self):
+        url = FOLLOW_URL.format(self.xiaoweige.id)
+
+        # need to login to follow
+        response = self.anonymous_client.post(url)
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+        # need to POST to follow
+        response = self.gongzi_client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
+
+        # can't follow yourself
+        response = self.xiaoweige_client.post(url)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+        # follow successfully
+        response = self.gongzi_client.post(url)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+        # follow again
+        response = self.gongzi_client.post(url)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(response.data['duplicate'], True)
+
+        # follow the other way will create new friendship
+        count = Friendship.objects.count()
+        response = self.xiaoweige_client.post(FOLLOW_URL.format(self.gongzi.id))
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(Friendship.objects.count(), count + 1)
+
+    def test_unfollow(self):
+        url = UNFOLLOW_URL.format(self.xiaoweige.id)
+
+        # need to login to unfollow
+        response = self.anonymous_client.post(url)
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+        # need to POST to unfollow
+        response = self.gongzi_client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
+
+        # can't unfollow yourself
+        response = self.xiaoweige_client.post(url)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+        # unfollow successfully
+        Friendship.objects.create(from_user=self.gongzi, to_user=self.xiaoweige)
+        count = Friendship.objects.count()
+        response = self.gongzi_client.post(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data['deleted'], 1)
+        self.assertEqual(Friendship.objects.count(), count - 1)
+
+        # unfollow again
+        count = Friendship.objects.count()
+        response = self.gongzi_client.post(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data['deleted'], 0)
+        self.assertEqual(Friendship.objects.count(), count)
+
+    def test_followings(self):
+        url = FOLLOWINGS_URL.format(self.xiaoweige.id)
+
+        # POST is not allowed
+        response = self.anonymous_client.post(url)
+        self.assertEqual(response.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
+        # GET is allowed
+        response = self.anonymous_client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data['followings']), 3)
+        # make sure order by reversed create_at time
+        ts0 = response.data['followings'][0]['created_at']
+        ts1 = response.data['followings'][1]['created_at']
+        ts2 = response.data['followings'][2]['created_at']
+        self.assertEqual(ts0 > ts1, True)
+        self.assertEqual(ts1 > ts2, True)
+        self.assertEqual(
+            response.data['followings'][0]['user']['username'],
+            'xiaoweige_following2',
+        )
+        self.assertEqual(
+            response.data['followings'][1]['user']['username'],
+            'xiaoweige_following1',
+        )
+        self.assertEqual(
+            response.data['followings'][2]['user']['username'],
+            'xiaoweige_following0',
+        )
+
+    def test_followers(self):
+        url = FOLLOWERS_URL.format(self.xiaoweige.id)
+        # POST is not allowed
+        response = self.anonymous_client.post(url)
+        self.assertEqual(response.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
+
+        # GET
+        response = self.anonymous_client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual((len(response.data['followers'])), 2)
+
+        # check timestep
+        ts0 = response.data['followers'][0]['created_at']
+        ts1 = response.data['followers'][1]['created_at']
+        self.assertEqual(ts0 > ts1, True)
+        self.assertEqual(
+            response.data['followers'][0]['user']['username'],
+            'xiaoweige_follower1',
+        )
+        self.assertEqual(
+            response.data['followers'][1]['user']['username'],
+            'xiaoweige_follower0',
+        )
+
+
+
---
+++ friendships/api/views.py
@@ -0,0 +1,64 @@
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated,AllowAny
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from django.contrib.auth.models import User
+from friendships.models import Friendship
+from friendships.api.serializers import (
+    FollowerSerializer,
+    FollowingSerializer,
+    FriendshipSerializerForCreate
+)
+
+
+class FriendshipViewSet(viewsets.GenericViewSet):
+    queryset = User.objects.all()
+
+    @action(methods=['GET'], detail=True, permission_classes=[AllowAny])
+    def followers(self, request, pk):
+        friendships = Friendship.objects.filter(to_user_id=pk).order_by('-created_at')
+        serializer = FollowerSerializer(friendships, many=True)
+        return Response({
+            'followers': serializer.data
+        }, status=200,)
+
+    @action(methods=['GET'], detail=True, permission_classes=[AllowAny])
+    def followings(self, request, pk):
+        friendships = Friendship.objects.filter(from_user_id=pk).order_by('-created_at')
+        serializer = FollowingSerializer(friendships, many=True)
+        return Response({
+            'followings': serializer.data
+        }, status=200,)
+
+    @action(methods=['POST'], detail=True, permission_classes=[IsAuthenticated])
+    def follow(self, request, pk):
+        if Friendship.objects.filter(from_user=request.user, to_user=pk).exists():
+            return Response({
+                'success': True,
+                'duplicate': True,
+            }, status=201)
+        serializer = FriendshipSerializerForCreate(data={
+            'from_user_id': request.user.id,
+            'to_user_id': pk,
+        })
+        if not serializer.is_valid():
+            return Response({
+                'success': False,
+                'errors': serializer.errors,
+            }, status=400)
+        serializer.save()
+        return Response({'success': True}, status=201)
+
+    @action(methods=['POST'], detail=True, permission_classes=[IsAuthenticated])
+    def unfollow(self, request, pk):
+        if request.user.id == int(pk):
+            return Response({
+                'success': False,
+                'message': 'You cannot unfollow yourself',
+            }, status=400)
+        deleted, _ = Friendship.objects.filter(
+            from_user=request.user,
+            to_user=pk,
+        ).delete()
+        return Response({'success': True, 'deleted': deleted})
+

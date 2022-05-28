5e51568ad93922a12d083e4faedf6665937e2243
RuihaoWei95 <74844130+RuihaoWei@users.noreply.github.com>
2022-05-28 01:53:54 -0700

    9-newsfeeds-model-api-and-tests

---
+++ newsfeeds/api/tests.py
@@ -0,0 +1,57 @@
+from newsfeeds.models import NewsFeed
+from friendships.models import Friendship
+from testing.testcases import TestCase
+from rest_framework.test import APIClient
+from rest_framework import status
+
+NEWSFEEDS_URL = '/api/newsfeeds/'
+POST_TWEETS_URL = '/api/tweets/'
+FOLLOW_URL = '/api/friendships/{}/follow/'
+
+
+class NewsFeedApiTest(TestCase):
+
+    def setUp(self):
+        self.anonymous_client = APIClient()
+
+        self.gongzi = self.create_user('gongzi')
+        self.gongzi_client = APIClient()
+        self.gongzi_client.force_authenticate(self.gongzi)
+
+        self.xiaoweige = self.create_user('xiaoweige')
+        self.xiaoweige_client = APIClient()
+        self.xiaoweige_client.force_authenticate(self.xiaoweige)
+
+        # create following and followers
+        for i in range(2):
+            follower = self.create_user(f'xiaoweige_follower{i}')
+            Friendship.objects.create(from_user=follower, to_user=self.xiaoweige)
+        for i in range(3):
+            following = self.create_user(f'xiaoweige_following{i}')
+            Friendship.objects.create(from_user=self.xiaoweige, to_user=following)
+
+    def test_list(self):
+        # test login account
+        response = self.anonymous_client.get(NEWSFEEDS_URL)
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+        # test POST
+        response = self.gongzi_client.post(NEWSFEEDS_URL)
+        self.assertEqual(response.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
+
+        # initial state, has nothing
+        response = self.gongzi_client.get(NEWSFEEDS_URL)
+        self.assertEqual(len(response.data['newsfeeds']), 0)
+
+        self.gongzi_client.post(POST_TWEETS_URL, {'content': 'Hello Gongzi'})
+        response = self.gongzi_client.get(NEWSFEEDS_URL)
+        self.assertEqual(len(response.data['newsfeeds']), 1)
+
+        self.gongzi_client.post(FOLLOW_URL.format(self.xiaoweige.id))
+        response = self.xiaoweige_client.post(POST_TWEETS_URL, {
+            'content': 'Hello Gongzi',
+        })
+        posted_tweet_id = response.data['id']
+        response = self.gongzi_client.get(NEWSFEEDS_URL)
+        self.assertEqual(len(response.data['newsfeeds']), 2)
+        self.assertEqual(response.data['newsfeeds'][0]['tweet']['id'], posted_tweet_id)
---
+++ newsfeeds/api/views.py
@@ -0,0 +1,21 @@
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from django.contrib.auth.models import User
+from newsfeeds.models import NewsFeed
+from newsfeeds.api.serializers import NewsFeedSerializer
+from rest_framework import status
+
+
+class NewsFeedViewSet(viewsets.GenericViewSet):
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        return NewsFeed.objects.filter(user=self.request.user)
+
+    def list(self, request):
+        serializer = NewsFeedSerializer(self.get_queryset(), many=True)
+        return Response({
+            'newsfeeds': serializer.data,
+        }, status=status.HTTP_200_OK)

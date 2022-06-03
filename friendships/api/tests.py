222751f7871d0db3d366bc171a029d0ea3c0d544
RuihaoWei95 <74844130+RuihaoWei@users.noreply.github.com>
2022-06-02 22:14:59 -0700

    11-comments-create-api

--- friendships/api/tests.py
+++ friendships/api/tests.py
@@ -12,7 +12,6 @@ FOLLOWERS_URL = '/api/friendships/{}/followers/'
 class FriendshipApiTest(TestCase):
 
     def setUp(self):
-        self.anonymous_client = APIClient()
 
         self.gongzi = self.create_user('gongzi')
         self.gongzi_client = APIClient()

df6d6f17170a0d48882bb2a14cfd75cf44ec7549
RuihaoWei95 <74844130+RuihaoWei@users.noreply.github.com>
2022-07-15 21:42:54 -0700

    add friendship and newsfeed pagination

--- friendships/services.py
+++ friendships/services.py
@@ -1,12 +1,17 @@
 from friendships.models import Friendship
 
 
-class FriendshipServices(object):
-
-
+class FriendshipService(object):
     @classmethod
     def get_followers(cls, user):
         friendships = Friendship.objects.filter(
             to_user=user,
         ).prefetch_related('from_user')
         return [friendship.from_user for friendship in friendships]
+
+    @classmethod
+    def has_followed(cls, from_user, to_user):
+        return Friendship.objects.filter(
+            from_user=from_user,
+            to_user=to_user,
+        ).exists()
